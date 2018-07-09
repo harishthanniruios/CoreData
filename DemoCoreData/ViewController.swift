e937413f44169b21941b0cd32214a0a6bc82ee4a
Kav Interactive <kavinteractive@gmail.com>
2018-07-09 18:18:28 +0530

    Initial Commit

---
+++ DemoCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoCoreData
+//
+//  Created by Kav Interactive on 09/07/18.
+//  Copyright © 2018 Kav Interactive. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
